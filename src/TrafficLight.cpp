3c9a179e8348cb7cdb9ce778df75ca91dc67c419
jsoulis <jtsoulis7@gmail.com>
2019-07-31 23:58:55 +0000

    Completed project for concurrency class of cpp nanodegree

---
+++ src/TrafficLight.cpp
@@ -0,0 +1,108 @@
+#include <iostream>
+#include <random>
+#include "TrafficLight.h"
+
+/* Implementation of class "MessageQueue" */
+
+ 
+template <typename T>
+T MessageQueue<T>::receive()
+{
+    // FP.5a : The method receive should use std::unique_lock<std::mutex> and _condition.wait() 
+    // to wait for and receive new messages and pull them from the queue using move semantics. 
+    // The received object should then be returned by the receive function. 
+    std::unique_lock<std::mutex> lck(_mutex);
+    _cond.wait(lck, [this] {return !_queue.empty();});
+
+    T msg = std::move(_queue.back());
+    _queue.pop_back();
+
+    return msg;
+}
+
+template <typename T>
+void MessageQueue<T>::send(T &&msg)
+{
+    // FP.4a : The method send should use the mechanisms std::lock_guard<std::mutex> 
+    // as well as _condition.notify_one() to add a new message to the queue and afterwards send a notification.
+    std::lock_guard<std::mutex> lck(_mutex);
+    _queue.push_back(std::move(msg));
+    _cond.notify_one();
+}
+
+/* Implementation of class "TrafficLight" */
+
+
+TrafficLight::TrafficLight()
+{
+    _currentPhase = TrafficLightPhase::red;
+}
+
+void TrafficLight::waitForGreen()
+{
+    // FP.5b : add the implementation of the method waitForGreen, in which an infinite while-loop 
+    // runs and repeatedly calls the receive function on the message queue. 
+    // Once it receives TrafficLightPhase::green, the method returns.
+    while(true) {
+        auto msg = _queue->receive();
+        if(msg == TrafficLightPhase::green) return;
+    }
+}
+
+TrafficLightPhase TrafficLight::getCurrentPhase()
+{
+    return _currentPhase;
+}
+
+void TrafficLight::simulate()
+{
+    // FP.2b : Finally, the private method „cycleThroughPhases“ should be started in a thread when the public method „simulate“ is called. To do this, use the thread queue in the base class. 
+    threads.emplace_back(std::thread(&TrafficLight::cycleThroughPhases, this));
+}
+
+// virtual function which is executed in a thread
+void TrafficLight::cycleThroughPhases()
+{
+    // FP.2a : Implement the function with an infinite loop that measures the time between two loop cycles 
+    // and toggles the current phase of the traffic light between red and green and sends an update method 
+    // to the message queue using move semantics. The cycle duration should be a random value between 4 and 6 seconds. 
+    // Also, the while-loop should use std::this_thread::sleep_for to wait 1ms between two cycles. 
+
+    //initialize stopwatch
+    std::chrono::time_point<std::chrono::system_clock> lastUpdate;
+    lastUpdate = std::chrono::system_clock::now();
+
+    std::default_random_engine gen;
+    std::uniform_int_distribution<int> distr(4000,6000);
+    double cycleDuration = distr(gen);
+    std::cout<<"cycle duration: " << cycleDuration <<std::endl;
+    std::cout<<"before cyclthroughphases while loop"<<std::endl;
+    while(true) {
+        //sleep at every iteration to reduce CPU Usage
+        std::this_thread::sleep_for(std::chrono::milliseconds(1));
+
+        long timeSinceLastUpdate = std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::system_clock::now() - lastUpdate).count();
+
+        if(timeSinceLastUpdate >= cycleDuration) {
+            
+            //adjust cycle length for next loop
+            cycleDuration = distr(gen);
+            
+            //toggle current phase of the traffic light
+            if(getCurrentPhase() == TrafficLightPhase::red) {
+                _currentPhase = TrafficLightPhase::green;
+            }
+            else {
+                _currentPhase = TrafficLightPhase::red;
+            }
+            //send update method to message queue 
+            _queue->send(std::move(_currentPhase));
+
+            //reset stop watch for next cycle
+            lastUpdate = std::chrono::system_clock::now();
+        }
+        
+    }
+    std::cout<<"after cyclthroughphases while loop"<<std::endl;
+}
+
